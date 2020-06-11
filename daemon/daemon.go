1098f2ccc66ff3d4893129fc98a50fea25b7378a
vincent <weiwei1379@126.com>
2020-06-11 10:15:27 +0800

    adjust daemon

--- daemon/daemon.go
+++ daemon/daemon.go
@@ -1,7 +1,7 @@
 package daemon
 
 import (
-	"fmt"
+	"log"
 	"os"
 	"os/exec"
 )
@@ -14,28 +14,22 @@ func IsDaemonMode() bool {
 
 func init() {
 	argc := len(os.Args)
-	if argc == 1 {
-		// redirectStderr()
-	} else if argc >= 2 {
+	if argc >= 2 {
 		if os.Args[1] == "--daemon=true" {
 			os.Args[1] = "--daemon"
 			cmd := exec.Command(os.Args[0], os.Args[1:]...)
-			cmd.Start()
-			fmt.Println("Server running in daemon . [PID]", cmd.Process.Pid)
+			err := cmd.Start()
+			if err != nil {
+				log.Fatal(err)
+				os.Exit(4)
+			}
+			log.Println("Server running in daemon . [PID]", cmd.Process.Pid)
 
-			os.Exit(0)
-		} else if os.Args[1] == "--daemon" {
-			os.Stdin.Close()
-			os.Stdout.Close()
 			isDaemon = true
-			redirectStderr()
-			i := 1
-			os.Args = append(os.Args[:i], os.Args[i+1:]...) // 恢复参数位置
-			fmt.Println("Daemon Server Initializing...")
-		} else {
+			os.Exit(0)
 		}
 	} else {
-		fmt.Println("The number of arguments incorrect .")
-		os.Exit(-1)
+		log.Println("The number of arguments incorrect .")
+		os.Exit(4)
 	}
 }
